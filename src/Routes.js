da2f043e693116f586f7808ee5eb76fb0faee956
asiyanbi mubashir <asiyanbimubashir@gmail.com>
2021-10-13 18:50:43 +0100

    WIP: product description page

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import {Switch,Route} from 'react-router-dom'
 //pages
 import Home from './pages/Home'
 import Cart from './pages/Cart'
+import ProductDescPage from './pages/ProductDescPage'
 
 export default class Routes extends Component {
     render() {
         return (
             <Switch>
-              <Route to='/cart'  component={Cart} />  
+              <Route to='/cart'   component={Cart} />  
+              <Route to='/product'  component={ProductDescPage} />  
               <Route to='/' exact component={Home} />  
             </Switch>
         )
---
+++ src/pages/ProductDescPage/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class index extends React.Component {
+constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+}
+
+    render() {
+        return (
+        <div>
+            ProductDescPage
+        </div>
+        )
+    }
+}
+
+
+export default index;
