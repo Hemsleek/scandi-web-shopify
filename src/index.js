75a3de08a4d63844dcd8fac3e632b8cb03b6cc5e
asiyanbi mubashir <asiyanbimubashir@gmail.com>
2021-10-14 18:00:09 +0100

    feat: apollo client setup

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { ApolloProvider } from "@apollo/client"
+
 import store from './store';
+import client from './Apollo';
 
 import App from './App';
 
+
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
